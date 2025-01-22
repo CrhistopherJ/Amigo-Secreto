69c902188b4dba7f45721d52c0b3a5ac3e5f37f5
Crhistopher JP <cijuarezp@gmail.com>
2025-01-22 17:04:29 -0600

    corrección de mensaje y añadir a amigos[]

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ let amigos=[];
 
 
 //función para ingresar nombres
-function agregarAmigo(listaAmigos){
-    let nombre=document.getElementById('amigo').value;
+function agregarAmigo(){
+    let nombre=(document.getElementById('amigo').value);
+    //valida la entrada
     if(nombre === ""){
-        alert("Debe ingresar un nombre, vuelva a intentarlo")
+        alert("Por favor, inserte un nombre")
     } else{
         let nombreLista=document.createElement('li');
+        //se actualiza amigos[]
+        amigos.push(nombre);
+        console.log(amigos);
+        //se añade el amigo al html
         nombreLista.textContent=nombre;
         document.getElementById('listaAmigos').appendChild(nombreLista);
+        
+        //se limpio la cadena de entrada
         document.getElementById('amigo').value="";
     }
 }
+
+
 
 
