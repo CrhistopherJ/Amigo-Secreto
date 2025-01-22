a3002e4c9930a70cc3ba6a1b25926f4057641f0b
Crhistopher JP <cijuarezp@gmail.com>
2025-01-22 16:49:34 -0600

    Funcion agrega amigo funcional

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+
+
+//función para ingresar nombres
+function agregarAmigo(listaAmigos){
+    let nombre=document.getElementById('amigo').value;
+    if(nombre === ""){
+        alert("Debe ingresar un nombre, vuelva a intentarlo")
+    } else{
+        let nombreLista=document.createElement('li');
+        nombreLista.textContent=nombre;
+        document.getElementById('listaAmigos').appendChild(nombreLista);
+        document.getElementById('amigo').value="";
+    }
+}
+
+
