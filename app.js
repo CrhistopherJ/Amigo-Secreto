4cc79425956c471f310e7be4efd95d121e818827
Crhistopher JP <cijuarezp@gmail.com>
2025-01-22 18:28:55 -0600

    Se separó la función para cumplir con la especificación y se terminó el requerimiento de Sortear amigo. Las 3 funciones estan completas.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos=[];
 
-
+/*
 //función para ingresar nombres
 function agregarAmigo(){
     let nombre=(document.getElementById('amigo').value);
@@ -12,7 +12,6 @@ function agregarAmigo(){
         let nombreLista=document.createElement('li');
         //se actualiza amigos[]
         amigos.push(nombre);
-        console.log(amigos);
         //se añade el amigo al html
         nombreLista.textContent=nombre;
         document.getElementById('listaAmigos').appendChild(nombreLista);
@@ -21,6 +20,47 @@ function agregarAmigo(){
         document.getElementById('amigo').value="";
     }
 }
+    */
+
+    //separando la función como lo pide el proyecto
+function actualizaLista(){
+    let lista=document.getElementById('listaAmigos');
+    lista.innerHTML="";
+    for(i=0;i<amigos.length;i++){
+        lista.innerHTML+="<li>"+amigos[i]+"</li>\n";
+    }
+        
+}
+
+
+function agregarAmigo(){
+    let nombre=document.getElementById('amigo').value;
+    //validar la entrada
+    if(nombre===""){
+        alert("Por favor, inserte un nombre");
+    }else{
+        amigos.push(nombre);
+        document.getElementById('amigo').value="";
+        actualizaLista();        
+    }
+}
+
+function sortearAmigo(){
+    let numeroDeAmigos=amigos.length;
+    if(numeroDeAmigos==0){
+        alert("No hay amigos disponibles, ingresa por lo menos un amigo");
+    }else{
+        let numeroGanador, amigoGanador, resultado;
+        numeroGanador=parseInt(Math.floor(Math.random()*numeroDeAmigos));
+        console.log(numeroGanador);
+        amigoGanador=amigos[numeroGanador];
+        resultado=document.getElementById('resultado');
+        resultado.innerHTML="El ganador es: "+ amigoGanador;
+    }
+
+}
+
+
 
 
 
